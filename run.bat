dfc21f5303c3e5afba117c5b63376a1f20c68145
NoneDudeXCX <nuho.hasan10@gmail.com>
2025-08-19 02:25:17 +0300

    Windows support

--- run.bat
+++ run.bat
@@ -5,3 +5,4 @@ pause
 REM use this to run the main Python script if your on windows
 REM if you are on Linux or Mac, run 'run.sh' instead
 REM make sure to have Python installed and added to your PATH
+REM I am preety sure this works though i havn't tested on windows yet
